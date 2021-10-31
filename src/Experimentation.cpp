10eb56afe1a8d70d9ff704902370003156bc7b1f
Pablo Lopes Perera <plopesperera99@gmail.com>
2021-10-31 20:33:35 -0300

    Ultimas cosas y dependencias de poetry

--- src/Experimentation.cpp
+++ src/Experimentation.cpp
@@ -6,7 +6,7 @@
 
 #define TOTAL_THREADS 10
 
-void experiment(std::string file_path){
+void experiment_1(std::string file_path){
     // Run functions with:
     // 1, 2, 4, 8, 16, 26, 32 and 64 threads
     int threads[TOTAL_THREADS] = { 1, 2, 4, 8, 12, 16, 26, 32, 48, 64 };
@@ -31,19 +31,29 @@ void experiment(std::string file_path){
             std::cout << threads[i] << "," << t << "," << (unsigned long) end_max.tv_nsec - start_max.tv_nsec << "," << "max" << "," << "-1" << std::endl;
         }
     }
+}
 
-        // for (int n = 1; n < 11; ++n){
-        //         file_paths.push_back(file_path);
+
+void experiment_2(std::string file_path){
+    clockid_t clk_id = CLOCK_REALTIME;
+    std::cout << "thread_count,id,time,function,file_count" << std::endl;
+    for (int j = 0; j < 50; ++j){
+        for (int i = 1; i < 21; i+=2){
+            HashMapConcurrente hashMap{};
+            std::vector<std::string> file_paths;
+            for (int n = 1; n < 11; ++n){
+                file_paths.push_back(file_path);
                 
-        //         struct timespec start_load, end_load;
-        //         clock_gettime(clk_id, &start_load);
-        //         cargarMultiplesArchivos(hashMap, threads[i], file_paths);
-        //         clock_gettime(clk_id, &end_load);
+                struct timespec start_load, end_load;
+                clock_gettime(clk_id, &start_load);
+                cargarMultiplesArchivos(hashMap, i, file_paths);
+                clock_gettime(clk_id, &end_load);
 
-        //         time_diff = (unsigned long) end_load.tv_nsec - start_load.tv_nsec;
-        //         std::cout << threads[i] << "," << t << "," << time_diff << "," << "load" << "," << n << std::endl;
-        //     }
-
+                long time_diff = (long) (end_load.tv_sec * 1000000000 + end_load.tv_nsec) - (start_load.tv_sec * 1000000000 + start_load.tv_nsec);
+                std::cout << i << "," << j << "," << time_diff << "," << "load" << "," << n << std::endl;
+            }
+        }
+    }
 }
 
 int main(int argc, char **argv) {
@@ -54,7 +64,8 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    experiment(argv[1]);
+    // experiment_1(argv[1]);
+    experiment_2(argv[1]);
     
     return 0;
 }
